bf242ad8e92712efbd13c556b77ee8eb2ed41dc7
vinhbui107 <vinhbui.tg@gmail.com>
2021-09-24 14:32:39 +0700

    add new model loaders

--- apps/common/model_loaders.py
+++ apps/common/model_loaders.py
@@ -3,3 +3,15 @@ from django.apps import apps
 
 def get_user_model():
     return apps.get_model("users", "User")
+
+
+def get_product_model():
+    return apps.get_model("products", "Product")
+
+
+def get_order_model():
+    return apps.get_model("orders", "Orders")
+
+
+def get_order_item_model():
+    return apps.get_model("orders", "OrderItem")
