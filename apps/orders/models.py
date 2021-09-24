a07a65e3f90f495b4f2917c29e3f505ae40ffba8
vinhbui107 <vinhbui.tg@gmail.com>
2021-09-24 14:32:01 +0700

    create new models

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -1,3 +1,56 @@
 from django.db import models
 
-# Create your models here.
+from apps.users.models import User
+from apps.products.models import Product
+
+
+ORDER_STATUS = (
+    ("0", "Awaiting Payment"),
+    ("1", "Shipped"),
+    ("2", "Completed"),
+    ("3", "Cancelled"),
+    ("4", "Refunded"),
+)
+
+
+class Order(models.Model):
+    buyer = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="orders"
+    )
+
+    amount = models.DecimalField(max_digits=12, decimal_places=2, default=0.0)
+    address = models.CharField(max_length=255, blank=True)
+    status = models.CharField(max_length=1, choices=ORDER_STATUS, default=0)
+    notes = models.TextField(blank=False, null=True)
+
+    created_at = models.DateTimeField(
+        editable=False, auto_now_add=True, db_index=True
+    )
+    modified_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "order"
+        verbose_name = "Order"
+        verbose_name_plural = "Orders"
+
+    def __str__(self):
+        return self.id
+
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+
+    price = models.DecimalField(max_digits=12, decimal_places=2, default=0.0)
+    quantity = models.PositiveIntegerField(default=0)
+
+    created_at = models.DateTimeField(editable=False, auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "order_item"
+        verbose_name = "OrderItem"
+        verbose_name_plural = "OrderItems"
+
+    def __str__(self):
+        return self.id
--- apps/products/models.py
+++ apps/products/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255, unique=True, db_index=True)
+
+    price = models.DecimalField(max_digits=12, decimal_places=2, default=0.0)
+    stock = models.PositiveIntegerField(default=0)
+    notes = models.TextField(blank=False, null=True)
+
+    created_at = models.DateTimeField(
+        editable=False, auto_now_add=True, db_index=True
+    )
+    modified_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "product"
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
+
+    def __str__(self):
+        return self.name
