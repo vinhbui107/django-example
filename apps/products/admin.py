0821ad1c8448643ea730fd6df9a8f38e474c035a
vinhbui107 <vinhbui.tg@gmail.com>
2021-09-27 14:35:55 +0700

    update admin site of products

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.db.models import Count, Q
+from django.db.models.aggregates import Sum
 
-# Register your models here.
+from apps.products.models import Product
+from apps.orders.models import Order
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "price",
+        "stock",
+        "is_active",
+        "notes",
+    )
+    search_fields = ("name",)
+
+    ordering = ("id",)
+    list_filter = ("is_active",)
+
+    readonly_fields = ["modified_at", "created_at"]
