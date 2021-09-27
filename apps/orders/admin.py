7a784f36417f94b20297ebce2e43751dde1ed82d
vinhbui107 <vinhbui.tg@gmail.com>
2021-09-27 14:34:54 +0700

    udate admin site of orders

--- apps/orders/admin.py
+++ apps/orders/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.orders.models import Order, OrderItem
+
+
+@admin.action(description="Update order status to Completed")
+def update_status_completed(modeladmin, request, queryset):
+    queryset.update(status=2)
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 3
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ["id", "buyer", "amount", "status", "notes"]
+    search_fields = ["id", "buyer__username"]
+    actions = [update_status_completed]
+
+    ordering = ["id"]
+    list_filter = ("status",)
+
+    fieldsets = (
+        ("Order's info", {"fields": ("buyer", "amount", "status")}),
+        ("Important dates", {"fields": ("modified_at", "created_at")}),
+    )
+
+    inlines = [OrderItemInline]
+
+    readonly_fields = ["buyer", "amount", "modified_at", "created_at"]
