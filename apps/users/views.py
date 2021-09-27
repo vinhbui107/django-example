e17a11081d68c6dc90e77c2b6a1d3dec0f0360a0
vinhbui107 <vinhbui.tg@gmail.com>
2021-09-27 14:38:53 +0700

    update api for autheticated user

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,10 +1,7 @@
-from django.db import transaction
-
 from rest_framework.views import APIView
 from rest_framework.exceptions import status
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import IsAuthenticated
 
-from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from apps.users.serializers import (
@@ -19,7 +16,6 @@ from apps.common.helpers import validate_data
 from apps.common.responses import (
     ApiSuccessResponse,
     ApiSuccessMessageResponse,
-    ApiErrorMessageResponse,
 )
 
 
@@ -42,22 +38,6 @@ class RegisterAPI(APIView):
         )
 
 
-# class Logout(APIView):
-#     def post(self, request):
-#         try:
-#             refresh_token = request.data["refresh"]
-#             token = RefreshToken(refresh_token)
-#             token.blacklist()
-
-#             return ApiSuccessMessageResponse(
-#                 message="Logout success", status=status.HTTP_205_RESET_CONTENT
-#             )
-#         except Exception as e:
-#             return ApiErrorMessageResponse(
-#                 message="Logout failed", status=status.HTTP_400_BAD_REQUEST
-#             )
-
-
 class UserAPI(APIView):
 
     permission_classes = (IsAuthenticated,)
@@ -65,12 +45,8 @@ class UserAPI(APIView):
     def get(self, request):
 
         user = request.user
-        user_serializer = UserInfoSerializer(
-            user, context={"request": request}
-        )
-        return ApiSuccessResponse(
-            data=user_serializer.data, status=status.HTTP_200_OK
-        )
+        user_serializer = UserInfoSerializer(user, context={"request": request})
+        return ApiSuccessResponse(data=user_serializer.data, status=status.HTTP_200_OK)
 
     def patch(self, request):
         data = validate_data(UpdateUserSerializer, request.data)
@@ -82,12 +58,8 @@ class UserAPI(APIView):
         user = request.user
         user.update(username=username, password=password, email=email)
 
-        user_serializer = UserInfoSerializer(
-            user, context={"request": request}
-        )
-        return ApiSuccessResponse(
-            data=user_serializer.data, status=status.HTTP_200_OK
-        )
+        user_serializer = UserInfoSerializer(user, context={"request": request})
+        return ApiSuccessResponse(data=user_serializer.data, status=status.HTTP_200_OK)
 
     def delete(self, request):
         data = validate_data(DeleteUserSerializer, request.data)
@@ -97,6 +69,4 @@ class UserAPI(APIView):
         user = request.user
         user.delete_with_password(password)
 
-        return ApiSuccessMessageResponse(
-            message="Goodbye 😔", status=status.HTTP_200_OK
-        )
+        return ApiSuccessMessageResponse(message="Goodbye 😔", status=status.HTTP_200_OK)
